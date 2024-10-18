21a8f87c3da56fb3e51bd2d473439b50b8a7bab1
acs0209 <annjae4676@naver.com>
2024-10-18 17:40:56 +0900

    Init project

---
+++ kidsping/src/main/java/com/kidsworld/kidsping/KidspingApplication.java
@@ -0,0 +1,13 @@
+package com.kidsworld.kidsping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidspingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidspingApplication.class, args);
+	}
+
+}
---
+++ kidsping/src/test/java/com/kidsworld/kidsping/KidspingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidsworld.kidsping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidspingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
