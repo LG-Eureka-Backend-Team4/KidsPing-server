cb9b743d3e855a87b24b9f8696216c2d4bf6c614
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-20 22:21:01 +0900

    Feat: 회원, 이벤트 엔티티 설계

--- kidsping/src/main/java/com/kidsworld/kidsping/domain/event/entity/EventParticipant.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/domain/event/entity/EventParticipant.java
@@ -1,22 +1,34 @@
 package com.kidsworld.kidsping.domain.event.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.kidsworld.kidsping.domain.user.entity.User;
+import com.kidsworld.kidsping.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class EventParticipant {
+@AllArgsConstructor
+@Builder
+public class EventParticipant extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "event_participant_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    private LocalDateTime registrationTime;
+    private boolean winingYn;
+
 }
