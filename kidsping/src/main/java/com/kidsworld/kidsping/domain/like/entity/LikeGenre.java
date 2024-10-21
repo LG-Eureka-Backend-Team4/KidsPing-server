fe7c4c1ba940f9fea78f6e0da5ef95b359969d2d
JUNHA CHOI <choijh0309@naver.com>
2024-10-21 11:50:47 +0900

    Feat : LikeGenre Entity

--- kidsping/src/main/java/com/kidsworld/kidsping/domain/like/entity/LikeGenre.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/domain/like/entity/LikeGenre.java
@@ -1,10 +1,10 @@
 package com.kidsworld.kidsping.domain.like.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.kidsworld.kidsping.domain.book.entity.Book;
+import com.kidsworld.kidsping.domain.kid.entity.Kid;
+import com.kidsworld.kidsping.domain.like.entity.enums.LikeStatus;
+import com.kidsworld.kidsping.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +12,24 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class LikeGenre {
+public class LikeGenre extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "like_genre_id")
     private Long id;
+
+    @Column(name= "is_deleted")
+    private boolean isDeleted;
+
+    @Enumerated(EnumType.STRING)
+    private LikeStatus likeStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "kid_id")
+    private Kid kid;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
 }
