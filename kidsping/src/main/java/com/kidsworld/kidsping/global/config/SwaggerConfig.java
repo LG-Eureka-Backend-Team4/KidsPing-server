4c07feb17e4626610a58697530b590dba70617b3
howecofe <am0229@naver.com>
2024-10-21 22:16:37 +0900

    Chore: Swagger 시큐리티 Authorize 설정

--- kidsping/src/main/java/com/kidsworld/kidsping/global/config/SwaggerConfig.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.kidsworld.kidsping.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +14,8 @@ public class SwaggerConfig {
     @Bean
     OpenAPI openApi() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme()))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .info(apiInfo());
     }
 
@@ -21,6 +24,13 @@ public class SwaggerConfig {
                 .title("키즈핑 API")
                 .description("키즈핑 REST API 명세 및 테스트")
                 .version("v1.0");
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
     }
 
 }
