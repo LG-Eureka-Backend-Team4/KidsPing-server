b458a5852fbd6618659f90d3cc9c1726a6dd4f04
JUNHA CHOI <choijh0309@naver.com>
2024-10-21 13:59:50 +0900

    Feat : GroupCode, CommonCode Entity

--- kidsping/src/main/java/com/kidsworld/kidsping/global/common/entity/GroupCode.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/global/common/entity/GroupCode.java
@@ -1,4 +1,24 @@
 package com.kidsworld.kidsping.global.common.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GroupCode {
+
+    @Id
+    @Column(name = "group_code", length = 3)
+    private String groupCode;
+
+    @Column(name = "group_code_name", length = 20)
+    private String groupCodeName;
+
+    @Column(name = "group_code_desc", length = 500)
+    private String groupCodeDesc;
 }
