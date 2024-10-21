97e4aa8f7373de757f99d2e403ebc6f44d69c276
acs0209 <annjae4676@naver.com>
2024-10-21 15:48:25 +0900

    Refactor: MBTI 성향 응답 엔티티 수정

--- kidsping/src/main/java/com/kidsworld/kidsping/domain/question/entity/MbtiQuestion.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/domain/question/entity/MbtiQuestion.java
@@ -1,17 +1,22 @@
 package com.kidsworld.kidsping.domain.question.entity;
 
+import com.kidsworld.kidsping.domain.mbti.entity.enums.PersonalityTrait;
 import com.kidsworld.kidsping.global.common.entity.BaseTimeEntity;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MbtiQuestion extends BaseTimeEntity {
 
@@ -20,8 +25,11 @@ public class MbtiQuestion extends BaseTimeEntity {
     @Column(name = "mbti_question_id")
     private Long id;
 
-    @Column(name = "mbti_question_content",length = 50)
+    @Column(name = "content", length = 50)
     private String content;
+
+    @Enumerated(EnumType.STRING)
+    private PersonalityTrait personalityTrait;
 
     @Column(name = "is_deleted")
     private Boolean isDeleted;
