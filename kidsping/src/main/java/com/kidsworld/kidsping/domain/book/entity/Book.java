3bbff14e77d95d8748df323b34a11218a4aed8bf
JUNHA CHOI <choijh0309@naver.com>
2024-10-22 17:39:53 +0900

    Refactor : Book Entity의 bookMBTI -> bookMbti

--- kidsping/src/main/java/com/kidsworld/kidsping/domain/book/entity/Book.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/domain/book/entity/Book.java
@@ -19,7 +19,7 @@ public class Book extends BaseTimeEntity{
 
     @OneToOne
     @JoinColumn(name = "mbti_id", nullable = false)
-    private BookMbti bookMBTI;
+    private BookMbti bookMbti;
 
     @ManyToOne
     @JoinColumn(name = "genre_id", nullable = false)
