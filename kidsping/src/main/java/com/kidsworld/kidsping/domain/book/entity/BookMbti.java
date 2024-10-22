00e7e7be3b7113ef309e694e3adf284df750ebea
JUNHA CHOI <choijh0309@naver.com>
2024-10-22 17:00:02 +0900

    Rename : BookMBTI -> BookMbti

--- kidsping/src/main/java/com/kidsworld/kidsping/domain/book/entity/BookMbti.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/domain/book/entity/BookMbti.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class BookMBTI extends BaseTimeEntity {
+public class BookMbti extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
