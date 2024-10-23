b997788849c94f572b9d7471a265e4013e62e6df
JUNHA CHOI <choijh0309@naver.com>
2024-10-23 13:50:40 +0900

    Refactor : Book 등록시 mbti 요소 및 강도 추가

---
+++ kidsping/src/main/java/com/kidsworld/kidsping/domain/book/repository/BookMbtiRepository.java
@@ -0,0 +1,7 @@
+package com.kidsworld.kidsping.domain.book.repository;
+
+import com.kidsworld.kidsping.domain.book.entity.BookMbti;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookMbtiRepository extends JpaRepository<BookMbti, Long> {
+}
