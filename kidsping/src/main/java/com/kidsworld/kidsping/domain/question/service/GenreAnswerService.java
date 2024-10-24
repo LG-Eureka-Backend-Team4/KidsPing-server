e40effc71e6c1700a297951c94abcbaf6e4b27bf
acs0209 <annjae4676@naver.com>
2024-10-24 20:48:09 +0900

    Refactor: 안쓰는 메서드 제거

--- kidsping/src/main/java/com/kidsworld/kidsping/domain/question/service/GenreAnswerService.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/domain/question/service/GenreAnswerService.java
@@ -5,10 +5,6 @@ import java.util.List;
 
 public interface GenreAnswerService {
 
-    GenreAnswerResponse getGenreAnswer(Long id);
-
-    List<GenreAnswerResponse> getGenreAnswersByKidId(Long kidId);
-
     /*
      * isDeleted 값이 true 이면서 날짜가 현재 기준으로 한달이 지난 id 값을 조회하는 메서드
      * */
