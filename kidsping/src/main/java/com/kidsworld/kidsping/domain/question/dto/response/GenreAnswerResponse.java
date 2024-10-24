0af4a87c022950a8c39fa352db91751c811ef0e8
JUNHA CHOI <choijh0309@naver.com>
2024-10-24 10:26:44 +0900

    Refactor : 장르 응답 내용 조회 API

--- kidsping/src/main/java/com/kidsworld/kidsping/domain/question/dto/response/GenreAnswerResponse.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/domain/question/dto/response/GenreAnswerResponse.java
@@ -9,16 +9,12 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class GenreAnswerResponse {
-    private Long id;
     private Long kidId;
-    private String content;
     private LocalDateTime createdAt;
 
     public static GenreAnswerResponse from(GenreAnswer genreAnswer) {
         return GenreAnswerResponse.builder()
-                .id(genreAnswer.getId())
                 .kidId(genreAnswer.getKid().getId())
-                .content(genreAnswer.getContent())
                 .createdAt(genreAnswer.getCreatedAt())
                 .build();
     }
