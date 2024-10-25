1632adc11d997158ad799b1c6b4dd7c56dbf43be
Sieun53 <tldms1176@hanmail.net>
2024-10-25 16:44:05 +0900

    Refactor: 스프링 시큐리티로 로그아웃 permit 수정

--- kidsping/src/main/java/com/kidsworld/kidsping/global/config/SecurityConfig.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/global/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/**",
+                                "/",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/api/users/**"
