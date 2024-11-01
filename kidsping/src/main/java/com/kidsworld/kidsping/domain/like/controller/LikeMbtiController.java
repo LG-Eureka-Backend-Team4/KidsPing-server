34f928989e17885c1598d0e350eb98449a3c1c6a
acs0209 <annjae4676@naver.com>
2024-11-01 10:35:01 +0900

    Refactor: 쿼리 파라미터 수정

--- kidsping/src/main/java/com/kidsworld/kidsping/domain/like/controller/LikeMbtiController.java
+++ kidsping/src/main/java/com/kidsworld/kidsping/domain/like/controller/LikeMbtiController.java
@@ -10,7 +10,7 @@ import com.kidsworld.kidsping.global.common.dto.ApiResponse;
 import com.kidsworld.kidsping.global.exception.ExceptionCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,8 +32,10 @@ public class LikeMbtiController {
     @GetMapping("/kid/{kidId}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public ResponseEntity<ApiResponse<Page<LikeBookResponse>>> getBooksLiked(@PathVariable("kidId") Long kidId,
-                                                                             Pageable pageable) {
-        return ApiResponse.ok(ExceptionCode.OK.getCode(), likeMbtiService.getBooksLiked(kidId, pageable),
+                                                                             @RequestParam(defaultValue = "0") int page,
+                                                                             @RequestParam(defaultValue = "10") int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return ApiResponse.ok(ExceptionCode.OK.getCode(), likeMbtiService.getBooksLiked(kidId, pageRequest),
                 ExceptionCode.OK.getMessage());
     }
 
